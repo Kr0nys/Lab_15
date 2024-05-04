eff9abf51c0223eddee7b0091a1361e383bd91ca
Kr0nys <144237820+Kr0nys@users.noreply.github.com>
2024-05-04 10:12:25 +0300

    Add files via upload

---
+++ zad2.2.pas
@@ -0,0 +1,42 @@
+﻿type
+  PN = ^Node; Node = record
+    data: integer; massiv: PN;
+  end;
+
+var
+  head, current, newNode: PN;
+  i: integer;
+
+begin
+  new(head);
+  current := head;
+  
+  for i := 1 to 10 do
+  begin
+    new(newNode);
+    newNode^.data := Random(100,1); // Генерируем случайное число от 0 до 99
+ 
+    current^.massiv := newNode;
+    current := newNode;
+  end;
+
+  write('Все элементы списка:');
+  current := head^.massiv;
+  while current <> nil do
+  begin
+    write(current^.data:3);
+    current := current^.massiv;
+  end;
+  writeln();
+  write('Четные элементы:');
+  
+  
+  current := head^.massiv;
+  while current <> nil do
+  begin
+    if current^.data mod 2 = 0 then
+      write(current^.data:3);
+    
+    current := current^.massiv;
+  end;
+end.
---
+++ zad2.3.pas
@@ -0,0 +1,46 @@
+﻿program lr15_2_3;
+
+type
+  PNode = ^Node;
+  Node = record 
+    Elem: Integer; 
+    Next: PNode; 
+  end;
+
+var
+  newNode, head, tail, pp: pNode; max, min, n: integer;
+
+begin
+  begin
+  for var i := 1 to 10 do
+  begin
+    new(newNode);
+    newNode^.elem := random(1, 100); 
+    newNode^.next := nil; 
+    if head = nil then head := newNode 
+    else
+    begin
+      tail := head;
+      while tail^.next <> nil do
+        tail := tail^.next;  
+      tail^.next := newNode; 
+    end;
+    write(newNode^.elem:3);
+  end;
+  
+  pp := head;
+  max := pp^.elem;
+  min := pp^.elem;
+  while (pp <> nil) do
+  begin
+    if (max < pp^.elem) then
+      max := pp^.elem;
+    if (min > pp^.elem) then
+      min := pp^.elem;
+    pp := pp^.next;
+  end;
+  Writeln;
+  Writeln('Максимальный элемент: ', max);
+  Writeln('Минимальный элемент: ', min);
+  end;
+end.
